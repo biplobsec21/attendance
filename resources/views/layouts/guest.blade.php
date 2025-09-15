5250c5934461fbcc12fbde280ca9a76d755c106a
biplobsec21 <biplobsec21@gmail.com>
2025-09-15 23:47:30 +0600

    Guest blade updated

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,9 +9,16 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        $css = asset('build/' . $manifest['resources/css/app.css']['file']);
+        $js = asset('build/' . $manifest['resources/js/app.js']['file']);
+    @endphp
 
+    <link rel="stylesheet" href="{{ $css }}">
+    <script src="{{ $js }}" defer></script>
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
