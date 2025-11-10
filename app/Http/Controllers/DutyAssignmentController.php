f610695626d6360f802301e5f5d683e0af0eebdc
biplobsec21 <biplobsec21@gmail.com>
2025-11-10 19:01:57 +0600

    For testing.

--- app/Http/Controllers/DutyAssignmentController.php
+++ app/Http/Controllers/DutyAssignmentController.php
@@ -68,7 +68,7 @@ class DutyAssignmentController extends Controller
     {
         $request->validate([
             'start_date' => 'required|date',
-            'end_date' => 'required|date|after_or_equal:start_date'
+            'end_date' => 'required|date' //|after_or_equal:start_date
         ]);
 
         try {
